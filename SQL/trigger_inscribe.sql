1744b749f7c845b79c728a3ea98dafd4a6a70ac6
Rodrigo <rodrigodemi@gmail.com>
2011-11-02 14:25:50 -0200

    el control de si ya aprobo va sobre rinde

--- SQL/trigger_inscribe.sql
+++ SQL/trigger_inscribe.sql
@@ -18,19 +18,7 @@ BEGIN
   	IF esta_disponible = 'N' THEN
 		RAISE_APPLICATION_ERROR(-20001,'El examen debe estar disponible para poder inscribirse a el.');
 	END IF;
-
-	--control de que un estudiante no se pueda volver a inscribir a un examen que ya lo tiene aprobado
-	select count(*) 
-	INTO cant_aprobadoExamen
-	FROM aprueba
-	WHERE 
-		:NEW.nro_examen = nro_examen
-	AND 
-		:NEW.nro_estudiante = nro_estudiante;
-
-	IF cant_aprobadoExamen > 0 THEN
-		RAISE_APPLICATION_ERROR(-20001,'El estudiante ya tiene aprobado el examen '|| :NEW.nro_examen ||'.');
-	END IF;
+	
 END;
 
 
@@ -43,22 +31,4 @@ INSERT INTO inscribe VALUES (1, 1);
 
 --en este insert se activa el trigger
 INSERT INTO inscribe VALUES (1, 2);
-
-
-
-
----DATOS DE PRUEBA PAR LA PARTE DE CONTROL DE INSCRIPCION SI YA ESTA APROBADO EL EXAMEN
-
-INSERT INTO estudiante VALUES (3, 3, 'Argentina', 'est3', 'apellido', 'asuncion');
-INSERT INTO examen VALUES (1, 'test de cooper', 'S');
-INSERT INTO examen VALUES (3, 'tiro al arco 2', 'S');
-INSERT INTO inscribe VALUES (3, 1);
-INSERT INTO inscribe VALUES (3, 3);
-
--- nro_estudiante , nro_examen, fecha, calificacion 
-INSERT INTO aprueba VALUES (3, 1, sysDate, 80);
-
---No debe permitirse:
-
-INSERT INTO inscribe VALUES (3, 1);
 
