224030bb46f331bc39c68f692d23832fea7f06b3
Mauricio <karbunkle@gmail.com>
2011-11-15 19:52:53 -0200

    informe examenes

--- SQL/informe_examenes.sql
+++ SQL/informe_examenes.sql
@@ -7,6 +7,17 @@ nombre_institucion VARCHAR2(50);
 fecha DATE;
 hora TIMESTAMP;
 pais VARCHAR2(30);
+nro_salon NUMBER;
+nro_silla NUMBER;
+nro_estudiante NUMBER;
+nombre_estudiante VARCHAR2(50);
+
+CURSOR cursor_instancias_examenes IS
+SELECT i.fecha
+FROM instancia_ex i
+WHERE i.nro_examen = nro_examen
+GROUP BY fecha
+ORDER BY fecha ASC;
 
 CURSOR cursor_examenes (par_fecha IN DATE) IS
 SELECT i.nombre_institucion, i.hora, t.pais
@@ -16,12 +27,15 @@ WHERE i.nro_examen = nro_examen
   AND t.nombre = i.nombre_institucion
 ORDER BY i.fecha ASC, i.hora DESC;
 
-CURSOR cursor_instancias_examenes IS
-SELECT i.fecha
-FROM instancia_ex i
-WHERE i.nro_examen = nro_examen
-GROUP BY fecha
-ORDER BY fecha ASC;
+CURSOR cursor_salon_alumno (par_nombre_inst IN VARCHAR2, par_fecha IN DATE) IS
+SELECT r.nro_salon, r.nro_silla_asignado, r.nro_estudiante, e.nombre
+FROM rinde r, estudiante e
+WHERE r.nro_examen = nro_examen
+  AND r.nro_estudiante = e.nro_estudiante
+  AND r.fecha = par_fecha
+  AND r.nombre_institucion = par_nombre_inst
+ORDER BY r.nro_salon ASC, r.nro_silla_asignado ASC;
+
 
 BEGIN
     
@@ -64,7 +78,21 @@ BEGIN
             BEGIN  
             
               DBMS_OUTPUT.PUT_LINE('----- ' || nombre_institucion || ' (' || pais || ') - ' || TO_CHAR(hora, 'HH24:MI'));
-                            
+              
+              OPEN cursor_salon_alumno(nombre_institucion, fecha);
+              LOOP
+                FETCH cursor_salon_alumno INTO nro_salon, nro_silla, nro_estudiante, nombre_estudiante;
+                  EXIT WHEN cursor_salon_alumno%NOTFOUND;
+                  
+                  BEGIN
+                  
+                    DBMS_OUTPUT.PUT_LINE('------- Salon: ' || nro_salon);
+                    
+                  END;
+                  
+              END LOOP;
+              CLOSE cursor_salon_alumno;
+                  
             END;
             
         END LOOP;
--- SQL/pruebas_informe_examenes.sql
+++ SQL/pruebas_informe_examenes.sql
@@ -8,6 +8,7 @@ BEGIN
   DELETE FROM instancia_ex WHERE 1=1;
   DELETE FROM inscribe WHERE 1=1;
   DELETE FROM institucion WHERE 1=1;
+  DELETE FROM salon WHERE 1=1;
   DELETE FROM estudiante WHERE 1=1;
   DELETE FROM examen WHERE 1=1;
   
@@ -15,6 +16,8 @@ BEGIN
   INSERT INTO estudiante VALUES (2983, 5, 'UY', 'Jose', 'Alvarez', 'La Paz');
   INSERT INTO estudiante VALUES (8945, 7, 'UY', 'Alicia', 'Perez', 'Lascano');
   INSERT INTO estudiante VALUES (9523, 9, 'UY', 'Jose', 'Rodriguez', 'Canelon chico');
+  INSERT INTO estudiante VALUES (3234, 11, 'CL', 'Antonio', 'Marquez', 'Pando');
+  INSERT INTO estudiante VALUES (3483, 15, 'CL', 'Silvia', 'Jumint', 'Empalme Olmos');
   
   INSERT INTO examen VALUES (142, 'FCE - First Certificate', 'S');
   
@@ -25,12 +28,21 @@ BEGIN
   INSERT INTO inscribe VALUES (2983, 142);
   INSERT INTO inscribe VALUES (8945, 142);
   INSERT INTO inscribe VALUES (9523, 142);
+  INSERT INTO inscribe VALUES (3234, 142);
+  INSERT INTO inscribe VALUES (3483, 142);
   
   INSERT INTO instancia_ex VALUES (142, 'Anglo', to_date('2011/08/15', 'yyyy/mm/dd'), to_date('2011-08-15 10:00:', 'YYYY-MM-DD HH24:MI:'));
   INSERT INTO instancia_ex VALUES (142, 'British School', to_date('2011/08/15', 'yyyy/mm/dd'), to_date('2011-08-15 09:00:', 'YYYY-MM-DD HH24:MI:'));
 
-  --informe_examenes(142, to_date('2011/01/01', 'yyyy/mm/dd'), to_date('2011/12/31', 'yyyy/mm/dd'));
-  
+  --TODO insertar salones
+
+  INSERT INTO rinde VALUES (142, 1234, 'Anglo', 1, to_date('2011/08/15', 'yyyy/mm/dd'), 1);
+  INSERT INTO rinde VALUES (142, 2983, 'Anglo', 1, to_date('2011/08/15', 'yyyy/mm/dd'), 2);
+  INSERT INTO rinde VALUES (142, 8945, 'Anglo', 23, to_date('2011/08/15', 'yyyy/mm/dd'), 11);
+  INSERT INTO rinde VALUES (142, 9523, 'Anglo', 23, to_date('2011/08/15', 'yyyy/mm/dd'), 12);
+  INSERT INTO rinde VALUES (142, 3234, 'British School', 23, to_date('2011/08/15', 'yyyy/mm/dd'), 8);
+  INSERT INTO rinde VALUES (142, 3483, 'British School', 123, to_date('2011/08/15', 'yyyy/mm/dd'), 12);
+
   informe_examenes(142, to_date('2011/07/01', 'yyyy/mm/dd'), NULL);
   
 END;
