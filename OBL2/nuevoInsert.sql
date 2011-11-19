46e60c278b8c296d3448e9044c4daeb3ffd2c59a
Rodrigo <rodrigodemi@gmail.com>
2011-11-18 23:13:36 -0200

    la posta

--- OBL2/nuevoInsert.sql
+++ OBL2/nuevoInsert.sql
@@ -27,28 +27,38 @@ BEGIN
     p_STATUS    := 1;
     nom_inst  := p_NOMBRE_INSTITUCION;
     
+    dbms_output.put_line('nom_inst='||nom_inst);
+    
     -- si no se pasa la institucion y nunca rindio el examen, da error 
     -- si no se pasa o si se pasa   y ya    rindio el examen, se retorna la ultima institucion en la que se rindio, la silla y el salon en que rindio.
     -- si    se pasa la institucion y nunca rindio el examen, retorna la institucion pasada, el primer salon y la primer silla de dicha institucion, para que luego se busque una libre, 
     BUSCAR_ULTIMO_SALON_SILLA(p_NRO_EXAMEN, p_NRO_ESTUDIANTE, p_FECHA, nom_inst, nro_salonFinal, nro_sillaFinal, estado_interno);
     
+    dbms_output.put_line('nro_salonFinal='||nro_salonFinal);
+    dbms_output.put_line('nro_sillaFinal='||nro_sillaFinal);
+    
    --BLOQUEO los rinde para la institucion seleccionada, para que nadie pueda leer ni escibir datos en rinde y relacionados con la institucion
-   --SELECT count(*)
---	INTO tmp
- --  FROM	rinde r
- --  WHERE 
---	r.nombre_institucion = nom_inst
---   AND	r.fecha = p_FECHA
---   FOR UPDATE;
+     dbms_output.put_line('BLOQUEO FOR UPDATE instancia examen(int, ex, fecha) ='
+          ||nom_inst||', '||p_NRO_EXAMEN||', '||p_FECHA);
+   
+   SELECT 1
+    INTO tmp
+   FROM	instancia_ex iex
+   WHERE 
+      iex.nombre_institucion = nom_inst
+   AND	iex.nro_examen = p_NRO_EXAMEN
+   AND iex.fecha    = p_FECHA
+   FOR UPDATE;
+    dbms_output.put_line('count= '||tmp);
+   
 
 --SET TRANSACTION ISOLATION LEVEL SERIALIZABLE NAME 'BUSCAR_SILLA_LIBRE_E_INSERTAR';
 	BEGIN
-
-   dbms_output.put_line('BUSCAR_SALON_SILLA('|| p_NOMBRE_INSTITUCION ||','||
+        dbms_output.put_line('BUSCAR_SALON_SILLA('|| p_NOMBRE_INSTITUCION ||','||
                    p_FECHA||','||
                    nro_salonFinal||','||
                    nro_sillaFinal||')');
-
+                   
 			BUSCAR_SALON_SILLA(
 				   p_NOMBRE_INSTITUCION,
 				   p_FECHA,
@@ -56,12 +66,12 @@ BEGIN
 				   nro_sillaFinal
 				  );
 	       
-	   INSERT INTO rinde VALUES (p_NRO_EXAMEN, p_NRO_ESTUDIANTE, nom_inst, nro_salonFinal,p_FECHA, nro_sillaFinal); 
-    COMMIT;
+	   INSERT INTO rinde VALUES (p_NRO_EXAMEN, p_NRO_ESTUDIANTE, nom_inst, nro_salonFinal, p_FECHA, nro_sillaFinal); 
+    -- COMMIT;
      dbms_output.put_line('VA a dormir 5 segs');
 	   DBMS_LOCK.sleep (5);
      dbms_output.put_line('termino de dormir hace commit');
-	--   COMMIT;
+	   COMMIT;
      dbms_output.put_line('hizo commit');
 	END;
    p_NRO_SALON := nro_salonFinal;
@@ -73,8 +83,10 @@ BEGIN
 EXCEPTION
     WHEN OTHERS THEN
     --	  call_status := DBMS_LOCK.RELEASE(lockhandle);
+    
+   dbms_output.put_line(  'CODE - '||SQLCODE||' -ERROR- '||SQLERRM);
     ROLLBACK;
     p_STATUS:=1;
-	  raise_application_error(-20001, SQLERRM);
+	  --raise_application_error(-20001, SQLERRM);
   
 END INSERT_RINDE;
