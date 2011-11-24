1b3bd0144cdfaee7fd97418eb4ab95200f3dda90
Rodrigo <rodrigodemi@gmail.com>
2011-11-23 23:29:28 -0200

    ahora si

---
+++ OBL2/PROCEDURE_FORMATO_UTC_A_LOCAL.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE PROCEDURE FORMATO_UTC_A_LOCAL 
+(
+  P_I_FECHA_HORA IN TIMESTAMP  
+, P_I_TIME_ZONE IN VARCHAR2  
+, P_O_FECHA_LOCAL OUT VARCHAR2  
+, P_O_HORA_LOCAL OUT VARCHAR2  
+) AS
+
+temp_timestamp TIMESTAMP;
+tmp_string varchar2(100);
+
+BEGIN
+
+
+tmp_string := TO_CHAR (P_I_FECHA_HORA) || P_I_TIME_ZONE;
+--dbms_output.put_line(tmp_string);
+
+temp_timestamp := SYS_EXTRACT_UTC (tmp_string);
+--dbms_output.put_line(temp_timestamp);
+
+P_O_FECHA_LOCAL := to_char(temp_timestamp, 'DD.Mon.YYYY');
+P_O_HORA_LOCAL  := LOWER(to_char(temp_timestamp, 'HH:MI PM')) ;
+
+END FORMATO_UTC_A_LOCAL;
+
