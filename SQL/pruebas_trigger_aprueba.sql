5fc05465f763d587eb5f3a7f60c1ef06eab75f53
Rodrigo <rodrigodemi@gmail.com>
2011-11-30 20:39:50 -0200

    Arreglo del trigger aprueba y agrega datos de prueba

--- SQL/pruebas_trigger_aprueba.sql
+++ SQL/pruebas_trigger_aprueba.sql
@@ -1,3 +1,11 @@
+DELETE FROM aprueba WHERE 1=1;
+DELETE FROM rinde WHERE 1=1;
+DELETE FROM instancia_ex WHERE 1=1;
+DELETE FROM inscribe WHERE 1=1;
+DELETE FROM salon WHERE 1=1;
+DELETE FROM institucion WHERE 1=1;
+DELETE FROM estudiante WHERE 1=1;
+DELETE FROM examen WHERE 1=1;
 
 --prueba para t1_aprueba
 INSERT INTO estudiante VALUES (1, 2, 'paraguay', 'est1', 'apellido', 'asuncion');
@@ -8,8 +16,27 @@ INSERT INTO inscribe VALUES (1, 1);
 INSERT INTO instancia_ex VALUES (1, 'inst1', to_date('2011/01/01', 'yyyy/mm/dd'), to_date('2011-12-02 16:00:', 'YYYY-MM-DD HH24:MI:'));
 INSERT INTO rinde VALUES (1, 1, 'inst1', 1, to_date('2011/01/01', 'yyyy/mm/dd'), 5);
 
---Apruebo un Examen 15 días después de la fecha en que se rindió.
+--Apruebo un Examen 15 días después de la fecha en que se rindió. (OK)
 INSERT INTO aprueba VALUES (1, 1, to_date('2011/01/15', 'yyyy/mm/dd'), 95);
 
---Apruebo un Examen 5 días después de la fecha en que se rindió.
+--Apruebo un Examen 5 días después de la fecha en que se rindió. (ERROR)
 INSERT INTO aprueba VALUES (1, 1, to_date('2011/01/05', 'yyyy/mm/dd'), 95);
+
+
+--CONTROL DE insersion en aprueba si no se rindio examen (ERROR)
+INSERT INTO estudiante VALUES (2222, 2222, 'UY', 'est2222', 'apellido2222', 'Maldonado');
+INSERT INTO aprueba VALUES (2222, 1, to_date('2011/01/15', 'yyyy/mm/dd'), 95);
+
+--control de que si hay 2 instancias de examen una posterior a la otra se 
+--compare con la ultima pa fecha del aprueba
+INSERT INTO instancia_ex VALUES (1, 'inst1', to_date('2011/11/01', 'yyyy/mm/dd'), to_date('2011/11/01 16:00:', 'yyyy/mm/dd HH24:MI:'));
+INSERT INTO rinde VALUES (1, 1, 'inst1', 1, to_date('2011/11/01', 'yyyy/mm/dd'), 5);
+
+-----(ERROR)
+INSERT INTO aprueba VALUES (1, 1, to_date('2011/11/02', 'yyyy/mm/dd'), 95);
+-----(OK)
+INSERT INTO aprueba VALUES (1, 1, to_date('2011/11/15', 'yyyy/mm/dd'), 95);
+
+
+
+
--- SQL/trigger_aprueba.sql
+++ SQL/trigger_aprueba.sql
@@ -1,20 +1,27 @@
-
-CREATE OR REPLACE TRIGGER t1_aprueba
+create or replace
+TRIGGER t1_aprueba
 BEFORE INSERT OR UPDATE OF nro_estudiante, nro_examen, fecha ON APRUEBA
 FOR EACH ROW
 DECLARE
   	fecha_instancia_examen DATE;
 BEGIN
-	SELECT ie.fecha INTO fecha_instancia_examen
+	SELECT MAX(ie.fecha) 
+  INTO fecha_instancia_examen
 	FROM instancia_ex ie, rinde r
  	WHERE 
 			:NEW.nro_estudiante = r.nro_estudiante
 		AND :NEW.nro_examen = r.nro_examen
 		AND ie.nro_examen = r.nro_examen
     AND ie.nombre_institucion = r.nombre_institucion;
-		
+    
+    --SI NO HAY INSTANCIA DE EXAMEN O SI NO HAY ALGUN RINDE LA FECHA QUEDA EN NULL
+   if fecha_instancia_examen is null THEN
+      RAISE_APPLICATION_ERROR(-20001,'NO SE RINDIO NINGUN EXAMEN, NO SE PUEDE APROBAR');
+   END IF; 
+   
   IF :NEW.fecha <= (fecha_instancia_examen + 7) THEN
-		RAISE_APPLICATION_ERROR(-20001,'La fecha de aprobacion de un examen debe ser al menos siete dias posterior a la fecha del mismo.');
+		RAISE_APPLICATION_ERROR(-20001,'La fecha de aprobacion de un examen debe ser al '||
+                'menos siete dias posterior a la fecha del mismo.(faltan '||((fecha_instancia_examen + 7) - :NEW.fecha +1)||' dias)');
 	END IF;
-  
+	
 END;
